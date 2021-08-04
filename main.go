7201df47ed1027bfd9bc8c5e53a6ee7420f830d2
Tamer Aly <tamer.aly@hellofresh.com>
2021-08-04 14:04:06 -0400

    add comments for clarity

--- main.go
+++ main.go
@@ -16,12 +16,20 @@ type (
 	}
 )
 
+// UnmarshalJSON note that this is a member function on the object
+// we wish to deserialize
 func (c *Country) UnmarshalJSON(data []byte) error {
+	// Normally, unmarshaling this struct would require it to be defined
+	// as a JSON object
+
+	// In our case, the country is defined as a string on the payload
+	// so we have to deserialize it as a string
 	var name string
 	if err := json.Unmarshal(data, &name); err != nil {
 		return err
 	}
 
+	// If deserialization is successful, assign it to the object
 	c.Name = name
 
 	return nil
